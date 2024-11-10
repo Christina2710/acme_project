edd92b0fb2b6e5b0798fcecf70ffecc1599b6e74
Кристина Барменкова <barmenkova.kristina@mail.ru>
2024-11-11 01:25:37 +0300

    до CBV

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from .models import Birthday
 
+
 class BirthdayForm(forms.ModelForm):
     class Meta:
         model = Birthday
         fields = '__all__'
     widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
-        } 
+        'birthday': forms.DateInput(attrs={'type': 'date'})
+    }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
